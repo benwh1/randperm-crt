2cdd9cc25a31e65a32d1dda4d48876ff2074600f
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-08-17 14:08:30 +0100

    Define `FactoredInteger` struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct FactoredInteger {
+    factors: Vec<(u8, u8)>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl FactoredInteger {
+    fn new(mut n: u64) -> Option<Self> {
+        let mut factors = Vec::new();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let pow2 = n.trailing_zeros() as u8;
+        if pow2 != 0 {
+            n >>= pow2;
+            factors.push((2, pow2));
+        }
+
+        for p in (3..u8::MAX).step_by(2) {
+            println!("{p} {factors:?}");
+            let q = p as u64;
+
+            let mut counter = 0;
+            while n % q == 0 {
+                counter += 1;
+                n /= q;
+            }
+
+            if counter > 0 {
+                factors.push((p, counter));
+            }
+
+            if n == 1 {
+                break;
+            }
+        }
+
+        if n == 1 {
+            Some(Self { factors })
+        } else {
+            None
+        }
     }
 }
